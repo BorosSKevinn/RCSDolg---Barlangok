906f37ceb9c38229d8522dcf171f737d2e5bdfb7
winec <winec@LAPTOP-IMFO5FQS>
2025-10-07 08:24:38 +0200

    Model létrehozása

---
+++ Models/Barlang.cs
@@ -0,0 +1,13 @@
+﻿namespace RCSDolg.Models
+{
+    public class Barlang
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; } = string.Empty;
+        public double Hossz { get; set; }
+        public double Kiterjedes { get; set; }
+        public double Melyseg { get; set; }
+        public double Magassag { get; set; }
+        public string Telepules { get; set; } = string.Empty;
+    }
+}
