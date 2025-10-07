a1c0f4d685b0f4d9ad14ec26671082a91c37dd56
winec <winec@LAPTOP-IMFO5FQS>
2025-10-07 08:46:33 +0200

    Index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -12,18 +12,39 @@ namespace RCSDolg.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly RCSDolg.Data.BarlangDbContext _context;
 
-        public IndexModel(RCSDolg.Data.BarlangDbContext context)
+        private readonly BarlangDbContext _context;
+
+        public IndexModel(BarlangDbContext context)
         {
             _context = context;
         }
 
-        public IList<Barlang> Barlang { get;set; } = default!;
+        public IList<Barlang> Barlang { get; set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchCity { get; set; }
+        public string? CurrentFilter { get; set; }
+        public List<string> Cities { get; set; } = new List<string>();
+
 
         public async Task OnGetAsync()
         {
             Barlang = await _context.Barlangok.ToListAsync();
+            IQueryable<Barlang> query = _context.Barlangok;
+
+            if (!string.IsNullOrEmpty(SearchCity))
+            {
+                query = query.Where(b => b.Telepules.Contains(SearchCity));
+                CurrentFilter = SearchCity;
+            }
+
+            Cities = await _context.Barlangok
+                .Select(b => b.Telepules)
+                .Distinct()
+                .OrderBy(t => t)
+                .ToListAsync();
+
+            Barlang = await query.ToListAsync();
         }
     }
 }
